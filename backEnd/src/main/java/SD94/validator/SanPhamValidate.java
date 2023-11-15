9003f6a3c64064774e41484042b84ac15ca094dd
h duong <honngduong25@gmail.com>
2023-11-15 18:18:14 +0700

    hduong

--- backEnd/src/main/java/SD94/validator/SanPhamValidate.java
+++ backEnd/src/main/java/SD94/validator/SanPhamValidate.java
@@ -7,54 +7,93 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class SanPhamValidate {
-    // Mua ngay
-    public static ResponseEntity<?> checkOut(SanPhamDTO sanPhamDTO) {
-        if (sanPhamDTO.getMaMauSac() == null || sanPhamDTO.getMaMauSac().trim().isEmpty()) {
-            Map<String, String> respone = new HashMap<>();
-            respone.put("Mess", "Phai chon mau sac va kich co");
-            return ResponseEntity.badRequest().body(respone);
-        }
 
-        if (sanPhamDTO.getKichCoDaChon() == null || sanPhamDTO.getKichCoDaChon().trim().isEmpty()) {
-            Map<String, String> respone = new HashMap<>();
-            respone.put("Mess", "Phai chon mau sac va kich co");
-            return ResponseEntity.badRequest().body(respone);
-        }
+    public enum ErrorCode {
+        Null_maMauSac,
 
-        if (sanPhamDTO.getSan_pham_id() == null) {
-            Map<String, String> respone = new HashMap<>();
-            respone.put("Mess", "Khong co san pham nao duoc tim thay");
-            return ResponseEntity.badRequest().body(respone);
-        }
+        Null_kichCoDaChon,
 
-        if (sanPhamDTO.getSoLuongHienCo() == null) {
-            Map<String, String> respone = new HashMap<>();
-            respone.put("Mess", "Khong co so luong hien co");
-            return ResponseEntity.badRequest().body(respone);
-        }
+        Null_san_pham_id,
 
-        if (sanPhamDTO.getSoLuong() == 0) {
-            Map<String, String> respone = new HashMap<>();
-            respone.put("Mess", "So luong khong duoc bo trong");
-            return ResponseEntity.badRequest().body(respone);
-        }
+        Null_soLuongDaChon,
 
-        if (sanPhamDTO.getSoLuong() <= 0) {
-            Map<String, String> respone = new HashMap<>();
-            respone.put("Mess", "So luong phai lon hon 0");
-            return ResponseEntity.badRequest().body(respone);
-        }
+        Null_donGia,
 
-        if (sanPhamDTO.getSoLuongHienCo() != null && sanPhamDTO.getSoLuong() > sanPhamDTO.getSoLuongHienCo()) {
-            Map<String, String> respone = new HashMap<>();
-            respone.put("Mess", "So luong chon khong duoc lon hon so luong dang co");
-            return ResponseEntity.badRequest().body(respone);
-        }
+        Null_soLuong,
 
-        return ResponseEntity.ok().build();
+        Null_tongTien,
+
+        Null_id_hoaDon,
+
+        Null_soLuongHienCo
+
     }
 
+    //Mua ngay
+    public static ResponseEntity<?> checkOut(SanPhamDTO sanPhamDTO) {
+        System.out.println(sanPhamDTO);
+        Map<String, String> errors = new HashMap<>();
+        checkMaMauSac(sanPhamDTO.getMaMauSac(), errors);
+        checkKichCoDaChon(sanPhamDTO.getKichCoDaChon(), errors);
+        checkSanPhamID(sanPhamDTO.getSan_pham_id(), errors);
+        checkSoLuongHienCo(sanPhamDTO.getSoLuongHienCo(), errors);
+        checkSoLuongDaChon(sanPhamDTO.getSoLuong(), sanPhamDTO.getSoLuongHienCo(), errors);
 
+        if (errors.isEmpty()) {
+            return ResponseEntity.ok().build();
+        }
 
+        return ResponseEntity.badRequest().body(errors);
 
+    }
+
+    public static void checkSoLuongHienCo(Integer SoLuongHienCO, Map<String, String> errors) {
+        if (SoLuongHienCO == null) {
+            errors.put(ErrorCode.Null_soLuongHienCo.name(), "Khong co so luong hien co");
+        }
+    }
+
+    public static void checkMaMauSac(String maMauSac, Map<String, String> errors) {
+        if (maMauSac == null || maMauSac.trim().isEmpty()) {
+            errors.put(ErrorCode.Null_maMauSac.name(), "Phai chon mau sac va kich co");
+        }
+    }
+
+    public static void checkKichCoDaChon(String kichCoDaChon, Map<String, String> errors) {
+        if (kichCoDaChon == null || kichCoDaChon.trim().isEmpty()) {
+            errors.put(ErrorCode.Null_kichCoDaChon.name(), "Phai chon mau sac va kich co");
+        }
+    }
+
+    public static void checkSoLuongDaChon(Integer soLuongDaChon, Integer soLuongDaCo, Map<String, String> errors) {
+        if (soLuongDaChon == null) {
+            errors.put(ErrorCode.Null_soLuongDaChon.name(), "So luong khong duoc bo trong");
+        }
+
+        if (soLuongDaChon <= 0) {
+            errors.put(ErrorCode.Null_soLuongDaChon.name(), "So luong phai lon hon 0");
+        }
+
+        if (soLuongDaCo != null && soLuongDaChon > soLuongDaCo) {
+            errors.put(ErrorCode.Null_soLuongDaChon.name(), "So luong chon khong duoc lon hon so luong dang co");
+        }
+    }
+
+    public static void checkDonGia(Integer donGia, Map<String, String> errors) {
+        if (donGia == null) {
+            errors.put(ErrorCode.Null_donGia.name(), "Don gia khong duoc bo trong");
+        }
+    }
+
+    public static void checkTongTien(Integer tongTien, Map<String, String> errors) {
+        if (tongTien == null) {
+            errors.put(ErrorCode.Null_tongTien.name(), "Tong tien khong duoc bo trong");
+        }
+    }
+
+    public static void checkSanPhamID(Long sanPham_id, Map<String, String> errors) {
+        if (sanPham_id == null) {
+            errors.put(ErrorCode.Null_san_pham_id.name(), "Khong co san pham nao duoc tim thay");
+        }
+    }
 }
