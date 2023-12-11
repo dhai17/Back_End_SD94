5a02af9dc638b194d897cb5e7f62824b8226e0d8
LINHNKPH22810\WIN <linhnkph22810@fpt.edu.vn>
2023-12-11 16:37:21 +0700

    Linh commit

--- backEnd/src/main/java/SD94/validator/SanPhamValidate.java
+++ backEnd/src/main/java/SD94/validator/SanPhamValidate.java
@@ -4,6 +4,7 @@ import SD94.dto.SanPhamDTO;
 import org.springframework.http.ResponseEntity;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class SanPhamValidate {
